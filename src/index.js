02bf34cf3ca7edfe7a50fb5299ea845a0dab8e01
Allen Liu <liuz@sandstudio.com>
2022-01-20 18:38:09 +0800

    update Google reCAPTCHA call method

--- src/index.js
+++ src/index.js
@@ -19,13 +19,60 @@
     }
     /**
      * @method Initialize JSSDK of Google reCAPTCHA v2
-     * @param {String} host => api hostname, default use "www.google.com". Specially, some areas require network proxying, such as China, just set host: "www.recaptcha.net".(optional)
+     * @param {Object} options => The configure options of Google reCAPTCHA.(required)
      */
-    GoogleReCaptcha.init = function(host) {
-        var domain = host || 'www.google.com';
+    GoogleReCaptcha.init = function(options) {
+        if (typeof options !== 'object' || options === null) {
+            console.error('Module "google-recaptcha-v2" error message => method "init" must pass a object variable, please check again!')
+            return;
+        }
+        // api hostname, default use "www.google.com". Specially, some areas require network proxying, such as China, just set host: "www.recaptcha.net".(optional)
+        GoogleReCaptcha.host = typeof options.host === 'string' ? options.host : 'www.google.com';
+        GoogleReCaptcha.id = typeof options.id === 'string' ? options.id : '';
+        GoogleReCaptcha.siteKey = typeof options.siteKey === 'string' ? options.siteKey : '';
+        GoogleReCaptcha.callback = typeof options.callback === 'function' ? options.callback : null;
+        GoogleReCaptcha.expiredCallback = typeof options.expiredCallback === 'function' ? options.expiredCallback : null;
+        GoogleReCaptcha.errorCallback = typeof options.errorCallback === 'function' ? options.errorCallback : null;
+        if (GoogleReCaptcha.id && GoogleReCaptcha.siteKey) {
+            // Whether the JSSDK is loaded successfully or not
+            GoogleReCaptcha.loaded = false
+            GoogleReCaptcha.setGlobalOnloadCallback();
+            GoogleReCaptcha.loadJSSDK();
+        }
+    }
+    /**
+     * @method Set the global onload callback function of Google reCAPTCHE
+     */
+    GoogleReCaptcha.setGlobalOnloadCallback = function() {
+        if (typeof window.GoogleReCaptchaOnloadCallback === 'function') return;
+        window.GoogleReCaptchaOnloadCallback = function() {
+            // console.log('Module "google-recaptcha-v2" debug message => the JSSDK of Google reCAPTCHA load successfully!');
+            GoogleReCaptcha.loaded = true;
+            grecaptcha.render(GoogleReCaptcha.id, {
+                'sitekey': GoogleReCaptcha.siteKey,
+                'size': 'invisible',
+                'callback': function(token) {
+                    GoogleReCaptcha.callback && GoogleReCaptcha.callback(token);
+                },
+                'expired-callback': function() {
+                    GoogleReCaptcha.expiredCallback && GoogleReCaptcha.expiredCallback();
+                },
+                'error-callback': function() {
+                    GoogleReCaptcha.errorCallback && GoogleReCaptcha.errorCallback({
+                        code: 10001,
+                        message: 'Google reCAPTCHA encounters an error(usually network connectivity), please try again!'
+                    });
+                }
+            })
+        }
+    }
+    /**
+     * @method Load the JSSDK of Google reCAPTCHA
+     */
+    GoogleReCaptcha.loadJSSDK = function() {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://' + domain + '/recaptcha/api.js';
+        script.src = 'https://' + GoogleReCaptcha.host + '/recaptcha/api.js?onload=GoogleReCaptchaOnloadCallback&render=explicit';
         script.async = 'async';
         script.defer = 'defer';
         document.body.appendChild(script);
@@ -36,6 +83,14 @@
      */
     GoogleReCaptcha.validate = function(event) {
         event.preventDefault();
+        if (!GoogleReCaptcha.loaded) {
+            // console.warn('Module "google-recaptcha-v2" warning message => the JSSDK of Google reCAPTCHA load unsuccessfully, please check again!');
+            GoogleReCaptcha.errorCallback && GoogleReCaptcha.errorCallback({
+                code: -1,
+                message: 'The JSSDK of Google reCAPTCHA load unsuccessfully, please check again!'
+            });
+            return;
+        }
         grecaptcha.reset();
         grecaptcha.execute();
     }
